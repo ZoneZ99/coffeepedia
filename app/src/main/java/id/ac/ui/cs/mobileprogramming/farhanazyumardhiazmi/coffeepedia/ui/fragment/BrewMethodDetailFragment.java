8b0424aed1bfae8fd437c318c196771d66dc16e5
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-11-20 02:09:06 +0700

    Brew method pages styling

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/farhanazyumardhiazmi/coffeepedia/ui/fragment/BrewMethodDetailFragment.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/farhanazyumardhiazmi/coffeepedia/ui/fragment/BrewMethodDetailFragment.java
@@ -27,98 +27,102 @@ import java.util.stream.Collectors;
 
 public class BrewMethodDetailFragment extends Fragment {
 
-    private FragmentBrewMethodDetailBinding mBinding;
+	private FragmentBrewMethodDetailBinding mBinding;
 
-    private BrewMethodViewModel mViewModel;
+	private BrewMethodViewModel mViewModel;
 
-    public static final String BREW_METHOD_ID = "brew_method_id";
+	public static final String BREW_METHOD_ID = "brew_method_id";
 
-    @Nullable
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater,
-            @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState
-    ) {
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_brew_method_detail, container, false);
-        mBinding.setCallback(mBrewMethodClickCallback);
-        return mBinding.getRoot();
-    }
+	@Nullable
+	@Override
+	public View onCreateView(
+		@NonNull LayoutInflater inflater,
+		@Nullable ViewGroup container,
+		@Nullable Bundle savedInstanceState
+	) {
+		mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_brew_method_detail, container, false);
+		mBinding.setCallback(mBrewMethodClickCallback);
+		return mBinding.getRoot();
+	}
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        BrewMethodViewModel.Factory factory = new BrewMethodViewModel.Factory(
-                requireActivity().getApplication(),
-                requireArguments().getLong(BREW_METHOD_ID)
-        );
-        mViewModel = new ViewModelProvider(this, factory).get(BrewMethodViewModel.class);
-        mBinding.setLifecycleOwner(getViewLifecycleOwner());
-        mBinding.setBrewMethodViewModel(mViewModel);
-        mBinding.buttonDeleteBrewMethod.setOnClickListener(mDeleteButtonOnClickCallback);
-        mBinding.buttonEditBrewMethod.setOnClickListener(mEditButtonOnClickCallback);
-        mBinding.buttonExportBrewMethodDetail.setOnClickListener(mExportButtonClickCallback);
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		BrewMethodViewModel.Factory factory = new BrewMethodViewModel.Factory(
+			requireActivity().getApplication(),
+			requireArguments().getLong(BREW_METHOD_ID)
+		);
+		mViewModel = new ViewModelProvider(this, factory).get(BrewMethodViewModel.class);
+		mBinding.setLifecycleOwner(getViewLifecycleOwner());
+		mBinding.setBrewMethodViewModel(mViewModel);
+		mBinding.buttonDeleteBrewMethod.setOnClickListener(mDeleteButtonOnClickCallback);
+		mBinding.buttonEditBrewMethod.setOnClickListener(mEditButtonOnClickCallback);
+		mBinding.buttonExportBrewMethodDetail.setOnClickListener(mExportButtonClickCallback);
 
-        mViewModel.getBrewMethodWithBrewRecipes().observe(
-                getViewLifecycleOwner(),
-                brewMethodWithBrewRecipes -> {
-                    List<String> brewRecipeNames = brewMethodWithBrewRecipes.getBrewRecipes().stream().map(BrewRecipe::getName).collect(Collectors.toList());
-                    mBinding.listRelatedBrewRecipes.setAdapter(
-                            new ArrayAdapter<>(
-                                    getContext(),
-                                    android.R.layout.simple_list_item_1,
-                                    brewRecipeNames
-                            )
-                    );
-                }
-        );
-    }
+		mViewModel.getBrewMethodWithBrewRecipes().observe(
+			getViewLifecycleOwner(),
+			brewMethodWithBrewRecipes -> {
+				if (brewMethodWithBrewRecipes.getBrewRecipes().isEmpty()) {
+					mBinding.titleRelatedBrewRecipes.setText(R.string.empty_related_brew_recipes);
+				} else {
+					List<String> brewRecipeNames = brewMethodWithBrewRecipes.getBrewRecipes().stream().map(BrewRecipe::getName).collect(Collectors.toList());
+					mBinding.listRelatedBrewRecipes.setAdapter(
+						new ArrayAdapter<>(
+							getContext(),
+							android.R.layout.simple_list_item_1,
+							brewRecipeNames
+						)
+					);
+				}
+			}
+		);
+	}
 
-    @Override
-    public void onDestroyView() {
-        mBinding = null;
-        super.onDestroyView();
-    }
+	@Override
+	public void onDestroyView() {
+		mBinding = null;
+		super.onDestroyView();
+	}
 
-    public static BrewMethodDetailFragment forBrewMethod(long brewMethodId) {
-        BrewMethodDetailFragment fragment = new BrewMethodDetailFragment();
-        Bundle args = new Bundle();
-        args.putLong(BREW_METHOD_ID, brewMethodId);
-        fragment.setArguments(args);
-        return fragment;
-    }
+	public static BrewMethodDetailFragment forBrewMethod(long brewMethodId) {
+		BrewMethodDetailFragment fragment = new BrewMethodDetailFragment();
+		Bundle args = new Bundle();
+		args.putLong(BREW_METHOD_ID, brewMethodId);
+		fragment.setArguments(args);
+		return fragment;
+	}
 
-    private final BrewMethodClickCallback mBrewMethodClickCallback = brewMethod -> {
-        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
-            Intent intent = new Intent(getActivity(), BrewRecipeFormActivity.class);
-            intent.putExtra(BrewRecipeFormActivity.IS_EDIT, false);
-            intent.putExtra(BREW_METHOD_ID, brewMethod.getBrewMethodId());
-            startActivity(intent);
-        }
-    };
+	private final BrewMethodClickCallback mBrewMethodClickCallback = brewMethod -> {
+		if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+			Intent intent = new Intent(getActivity(), BrewRecipeFormActivity.class);
+			intent.putExtra(BrewRecipeFormActivity.IS_EDIT, false);
+			intent.putExtra(BREW_METHOD_ID, brewMethod.getBrewMethodId());
+			startActivity(intent);
+		}
+	};
 
-    private final View.OnClickListener mDeleteButtonOnClickCallback = view -> {
-        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
-            BrewMethod deletedBrewMethod = mViewModel.getBrewMethodWithBrewRecipes().getValue().getBrewMethod();
-            mViewModel.deleteBrewMethod(deletedBrewMethod);
-            Toast.makeText(getContext(), R.string.success_delete_brew_method, Toast.LENGTH_SHORT).show();
-            getActivity().onBackPressed();
-        }
-    };
+	private final View.OnClickListener mDeleteButtonOnClickCallback = view -> {
+		if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+			BrewMethod deletedBrewMethod = mViewModel.getBrewMethodWithBrewRecipes().getValue().getBrewMethod();
+			mViewModel.deleteBrewMethod(deletedBrewMethod);
+			Toast.makeText(getContext(), R.string.success_delete_brew_method, Toast.LENGTH_SHORT).show();
+			getActivity().onBackPressed();
+		}
+	};
 
-    private final View.OnClickListener mEditButtonOnClickCallback = view -> {
-        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
-            BrewMethod editedBrewMethod = mViewModel.getBrewMethodWithBrewRecipes().getValue().getBrewMethod();
-            ((BrewMethodsActivity) getActivity()).startBrewMethodEditView(editedBrewMethod);
-        }
-    };
+	private final View.OnClickListener mEditButtonOnClickCallback = view -> {
+		if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+			BrewMethod editedBrewMethod = mViewModel.getBrewMethodWithBrewRecipes().getValue().getBrewMethod();
+			((BrewMethodsActivity) getActivity()).startBrewMethodEditView(editedBrewMethod);
+		}
+	};
 
-    private final View.OnClickListener mExportButtonClickCallback = view -> {
-        if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
-            mViewModel.getBrewMethodWithBrewRecipes().observe(this, brewMethodWithBrewRecipes -> ((BrewMethodsActivity) getActivity())
-                    .exportDataToPdf(
-                            brewMethodWithBrewRecipes.getBrewMethod().getBrewMethodId(),
-                            "brew_method_detail.pdf"
-                    ));
-        }
-    };
+	private final View.OnClickListener mExportButtonClickCallback = view -> {
+		if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+			mViewModel.getBrewMethodWithBrewRecipes().observe(this, brewMethodWithBrewRecipes -> ((BrewMethodsActivity) getActivity())
+				.exportDataToPdf(
+					brewMethodWithBrewRecipes.getBrewMethod().getBrewMethodId(),
+					"brew_method_detail.pdf"
+				));
+		}
+	};
 }
