bdd56cdb085e52247833190fca4a812943d2709c
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2021-01-02 02:22:11 +0700

    Remove unused import and onCreate override in CoffeePediaApplication

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/farhanazyumardhiazmi/coffeepedia/CoffeePediaApplication.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/farhanazyumardhiazmi/coffeepedia/CoffeePediaApplication.java
@@ -1,18 +1,12 @@
 package id.ac.ui.cs.mobileprogramming.farhanazyumardhiazmi.coffeepedia;
 
 import android.app.Application;
-import android.content.Context;
 import id.ac.ui.cs.mobileprogramming.farhanazyumardhiazmi.coffeepedia.data.CoffeePediaDatabase;
 import id.ac.ui.cs.mobileprogramming.farhanazyumardhiazmi.coffeepedia.data.repository.BrewMethodRepository;
 import id.ac.ui.cs.mobileprogramming.farhanazyumardhiazmi.coffeepedia.data.repository.BrewRecipeRepository;
 import id.ac.ui.cs.mobileprogramming.farhanazyumardhiazmi.coffeepedia.data.repository.CoffeeBeanRepository;
 
 public class CoffeePediaApplication extends Application {
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-	}
 
 	public CoffeePediaDatabase getDatabase() {
 		return CoffeePediaDatabase.getInstance(this);
